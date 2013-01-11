5220cd700f63c7087aff582762d9c4a6c0b2c4ca
Nick Lanng <nicholas.rosenorn-lanng@newvoicemedia.com>
2013-01-11 10:24:01 +0000

    Now showing all of the posts on the front page

--- app.js
+++ app.js
@@ -7,7 +7,17 @@ dependencies['express'] = require('express');
 dependencies['stylus'] = require('stylus');
 dependencies['nib'] = require('nib');
 
+var postService = require('./lib/post-service.js')
+postService.initialize(dependencies, function(err) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  dependencies['post-service'] = postService;
 
-var HttpServer = require('./lib/http-server');
-var httpServer = HttpServer(dependencies);
-httpServer.run(port);
+  var HttpServer = require('./lib/http-server');
+  var httpServer = HttpServer(dependencies);
+  httpServer.run(port);
+});
+
+
